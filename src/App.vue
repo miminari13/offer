b87f2a23c07b33bbc97b780ff54a41569577c0d7
Mir <miminari13@gmail.com>
2022-08-18 23:54:38 +0300

    Init app

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+  // This starter template is using Vue 3 <script setup> SFCs
+  // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+  import DashBoard from './components/DashBoard.vue'
+</script>
+
+<template>
+  <DashBoard msg="Let's start" />
+</template>
+
+<style scoped></style>
